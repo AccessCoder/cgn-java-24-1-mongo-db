9ea6a6d098838c325e4b8baa3bb749ef05be8919
Dominic <andro5@web.de>
2024-04-03 15:38:40 +0200

    add information for @id Annotation

--- src/main/java/de/neuefische/cgnjava241mongodb/Student.java
+++ src/main/java/de/neuefische/cgnjava241mongodb/Student.java
@@ -4,4 +4,13 @@ import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document("Students")
 public record Student(String id, String name, int age) {
+/**
+ * Heißt ein Feld explizit id, wird es durch Spring Data
+ * auf das _id Feld der MongoDB gesetzt.
+ * Haben wir mal Fälle in denen dies nicht zutrifft
+ * z. B. Bücher die mit isbn abgespeichert werden
+ * müssen wir das jeweilige Feld (z. B. isbn) mit
+ * @id annotieren, damit dies ebenfalls die automatisch
+ * generierte ID von MongoDB überschreibt
+ */
 }
